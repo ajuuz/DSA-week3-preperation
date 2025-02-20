f2a4076999cc3973060009f545adcbbaef698693
ajmalea <eagaming0007@gmail.com>
2025-02-20 15:07:58 +0530

    heap insertion

---
+++ Heap/Heap2.js
@@ -0,0 +1,43 @@
+// Now we have to insert a element to the heap
+
+
+class Heap2{
+    constructor(){
+        this.heap = [];
+    }
+
+    getParent(index){
+        return Math.floor((index-1)/2)
+    }
+
+    insert(value){
+        this.heap.push(value);
+        this.heapifyUp(this.heap.length-1);
+    }
+
+    heapifyUp(index){
+        if(index===0) return;
+
+        let parentIndex  = this.getParent(index);
+        if(this.heap[index] < this.heap[parentIndex])
+        {
+            [[this.heap[index]],[this.heap[parentIndex]]]=[[this.heap[parentIndex]],[this.heap[index]]];
+            this.heapifyUp(parentIndex)
+        }
+    }
+
+    display(){
+        console.log(this.heap)
+    }
+}
+
+
+const heap2 = new Heap2();
+heap2.insert(5)
+heap2.insert(10)
+heap2.insert(15)
+heap2.insert(20)
+heap2.insert(25)
+heap2.insert(30)
+heap2.insert(3)
+heap2.display()
